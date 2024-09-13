e0dabcbe4904354aa1e9c06759a405472d0f61e6
jorenham <jhammudoglu@gmail.com>
2024-09-13 03:03:54 +0200

    add imports to  `from typing[_extensions] import ...` & code cleanup

--- examples/pep695_type_alias.pyi
+++ examples/pep695_type_alias.pyi
@@ -1,4 +1,9 @@
-import os
+from os import PathLike as _PathLike
+from typing import LiteralString
 
-type PathLike[S: (str, bytes)] = S | os.PathLike[S]
+type AnyStr = str | bytes
+type PathLike[S: (str, bytes)] = S | _PathLike[S]
 type RPair[RT, LT=RT] = tuple[LT, RT]  # noqa: E225  # fmt: skip
+
+TMP_DIR: PathLike[LiteralString]
+TWO_STRINGS: RPair[str]
