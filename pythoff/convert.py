84d3722e6ecf763059656d50958ac6c31669d8f7
jorenham <jhammudoglu@gmail.com>
2024-09-15 01:45:19 +0200

    complete PEP-695 backporting

--- pythoff/convert.py
+++ pythoff/convert.py
@@ -5,22 +5,11 @@ from ._pep695 import PEP695Collector, TypeAliasTransformer, TypingImportTransfor
 __all__ = ("convert",)
 
 
-def convert(source: str, /, *, is_pyi: bool = True) -> str:
-    visitor = PEP695Collector(is_pyi=is_pyi)
-    module = cst.parse_module(source)
-
-    wrapper = cst.MetadataWrapper(module)
-    _ = wrapper.visit(visitor)
-
-    alias_transformer = TypeAliasTransformer(type_params=visitor.type_params)
-    module_out = wrapper.module.visit(alias_transformer)
-
-    import_transformer = TypingImportTransformer(
-        cur_imports_typing=visitor.cur_imports_typing,
-        req_imports_typing=visitor.req_imports_typing,
-        cur_imports_typing_extensions=visitor.cur_imports_typing_extensions,
-        req_imports_typing_extensions=visitor.req_imports_typing_extensions,
+def convert(source: str, /) -> str:
+    return (
+        cst.parse_module(source)
+        .visit(collector := PEP695Collector())
+        .visit(TypeAliasTransformer.from_collector(collector))
+        .visit(TypingImportTransformer.from_collector(collector))
+        .code
     )
-    module_out = module_out.visit(import_transformer)
-
-    return module_out.code
